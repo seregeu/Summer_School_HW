e950b2d395ba0efe7cbb0324ed84cd6bea4c3dcc
seregeu <vetrser@gmail.com>
2021-07-22 00:19:26 +0300

    Init

--- app/src/main/java/com/example/summer_school_hw/MainActivity.kt
+++ app/src/main/java/com/example/summer_school_hw/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.summer_school_hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.summer_school_hw.data.presentation.ActorsModel
+import com.example.summer_school_hw.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
