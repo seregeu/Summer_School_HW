90457669ba9505d12e02f487a23ed55e5fc6a694
seregeu <vetrser@gmail.com>
2021-07-22 07:28:08 +0300

    Main and one extra done

--- app/src/main/java/com/example/summer_school_hw/MainActivity.kt
+++ app/src/main/java/com/example/summer_school_hw/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.summer_school_hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.summer_school_hw.fragments.UserInfoFragment
+import com.example.summer_school_hw.ui.main.MovieDetailsFragment
 import com.example.summer_school_hw.ui.main.MovieListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +18,25 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragment_container, MovieListFragment.newInstance())
                 .commitNow()
         }
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MovieListFragment.newInstance())
+                        .commitNow()
+                }
+                R.id.nav_profile -> {
+                    val BACK_STACK_ROOT_TAG = "root_fragment"
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.fragment_container,
+                        UserInfoFragment.newInstance()).addToBackStack(BACK_STACK_ROOT_TAG).commit()
+                }
+                else -> null
+            } != null
+        }
     }
+
+
+
 }
